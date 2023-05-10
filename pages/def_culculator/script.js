9cb90f42f77b9133511886fb1c4655950d1a61ea
c2tr <80561604+c2tr-plugin@users.noreply.github.com>
2023-05-10 19:51:37 +0900

    upd

--- pages/def_culculator/script.js
+++ pages/def_culculator/script.js
@@ -1,28 +1,12 @@
-function readUserInput(question) {
-    const readline = require('readline').createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  
-    return new Promise((resolve, reject) => {
-      readline.question(question, (answer) => {
-        resolve(answer);
-        readline.close();
-      });
-    });
-  }
-
-  (async function main() {
-    const plv = await readUserInput('自キャラのレベルを入力してください。\n');
-    const elv = await readUserInput('敵のレベルを入力してください。\n');
-    const debuff = await readUserInput('敵への防御デバフの合計値で入力してください。(%)\n');
+function cul() {
+    const plv = document.querySelector("#input1").value;
+    const elv = document.querySelector("#input2").value;
+    const debuff = document.querySelector("#input3").value;
+    
     edef = ( 200 + ( elv * 10 )) * ((100 - debuff) / 100);
     defmul = 1 - ( edef / ( edef + 200 + ( 10 * plv)));
-    console.log(`\n敵の防御力 : ${edef}`);
-    console.log(`防御係数 : ${defmul}`);
-    let fin;
-    while (fin !== "y") {
-        fin = await readUserInput('\n終了しますか？(y/n)\n');
-    }
-  })();
-  //nexe test.js --target windows-x64-14.15.3 --output hsr_def_calculator.exe
+    
+    console.log(`Enemy Deffence : ${edef}`);
+    console.log(`Diffence Multiplier : ${defmul}`);
+}
+    
