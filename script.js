4d198f912271b1f0e2ceff825fc98eba5da04166
c2tr <80561604+c2tr-plugin@users.noreply.github.com>
2023-05-10 17:33:12 +0900

    init

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-const map_status = {
-    "Attack":["16", "N/A", "image/icon/Icon_Stat_Attack.png"],
-    "HP":["1010", "N/A", "image/icon/Icon_Stat_HP.png"],
-    "Crit":["12", "N/A", "image/icon/Icon_Stat_Crit.png"]
-};
+const list = [1, 2, 3, 4, 5]; //今回のバナー
 
-const status_table = document.querySelector("table.status");
+const map = {
+    1: ["Nilou", "-920px", "-140px"],
+    2: ["Nahida", "-900px", "-250px"],
+    3: ["Layla", "-930px", "-140px"],
+    4: ["Shinobu", "-990px", "-80px"],
+    5: ["Dori", "-960px", "-80px"]
+}
 
-for (const entry of Object.entries(map_status) ) {
-    const tr = document.createElement("tr");
-    const th = document.createElement("th");
-    const image = document.createElement("img");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
+for (let n = 1; n < 6; n++) {
+    const ch = document.querySelector(`.c${n}`);
 
-    image.setAttribute('src', entry[1][2]);
-
-    th.innerText = entry[0];
-    td1.innerText = entry[1][0];
-    td2.innerText = entry[1][1];
-
-    th.classList.add("row");
-    image.classList.add("status_icon");
-
-    tr.appendChild(th);
-    th.insertBefore(image, th.firstChild);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    status_table.appendChild(tr);
+    ch.style.right = `${n*3-3}px`;
+    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${map[list[n-1]][0]}.png)`;
+    ch.style.backgroundPosition = `${map[list[n-1]][1]} ${map[list[n-1]][2]}`;
 }
