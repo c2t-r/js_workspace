1922b516ce328ae21f4b69c35adafb480c043ea8
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-11 09:41:34 +0900

    Update script.js

--- util/def_culculator/script.js
+++ util/def_culculator/script.js
@@ -6,12 +6,15 @@ function cul() {
     edef = ( 200 + ( elv * 10 )) * ((100 - debuff) / 100);
     defmul = 1 - ( edef / ( edef + 200 + ( 10 * plv)));
     
-    edef0 = ( 200 + ( elv * 10 ));
-    defmul0 = 1 - ( edef0 / ( edef0 + 200 + ( 10 * plv)));
-    diff = defmul / defmul0 * 100;
+    if (debuff !== 0) {
+        edef0 = ( 200 + ( elv * 10 ));
+        defmul0 = 1 - ( edef0 / ( edef0 + 200 + ( 10 * plv)));
+        diff = defmul / defmul0 * 100;
+        document.querySelector("#result > p#diff").setAttribute("style", "visibility: visible;")
+        document.querySelector("#result > p#diff > a").textContent = diff;
+    }
     
     document.querySelector("#result > p#edef > a").textContent = edef;
     document.querySelector("#result > p#defmul > a").textContent = defmul;
-    document.querySelector("#result > p#diff > a").textContent = diff;
 }
     
