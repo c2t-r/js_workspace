e790aeae487a4692b2116ab708ded612119f8ab0
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-12 23:41:44 +0900

    add tooltip

--- util/def_culculator/script.js
+++ util/def_culculator/script.js
@@ -6,17 +6,21 @@ function cul() {
     
     edef = ( 200 + ( elv * 10 )) * ((100 - debuff) / 100);
     defmul = 1 - ( edef / ( edef + 200 + ( 10 * plv)));
+    edef0 = ( 200 + ( elv * 10 ));
+    defmul0 = 1 - ( edef0 / ( edef0 + 200 + ( 10 * plv)));
+    diff = defmul / defmul0 * 100 - 100;
     
     if (debuff != 0) {
-        edef0 = ( 200 + ( elv * 10 ));
-        defmul0 = 1 - ( edef0 / ( edef0 + 200 + ( 10 * plv)));
-        diff = defmul / defmul0 * 100 - 100;
-        document.querySelector(".tooltip_area").setAttribute("style", "visibility: visible;")
+        document.querySelector(".tooltip_area2").setAttribute("style", "visibility: visible;")
         document.querySelector("span#diff > a").textContent = diff;
     } else {
-        document.querySelector("#tooltip1").checked = false;
-        document.querySelector(".tooltip_area").setAttribute("style", "visibility: hidden;")
+        document.querySelector("#tooltip2").checked = false;
+        document.querySelector(".tooltip_area2").setAttribute("style", "visibility: hidden;")
     }
+
+    reduce = (0.5 / defmul0 * 100 - 100);
+    document.querySelector(".tooltip_area").setAttribute("style", "visibility: visible;")
+    document.querySelector("span#reduce > a").textContent = reduce;
     
     document.querySelector("#result > p#edef").setAttribute("style", "visibility: visible;")
     document.querySelector("#result > p#edef > a").textContent = edef;
