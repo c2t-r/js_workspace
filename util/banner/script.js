f70f2e2ff2250e1b202e9a2a76c69fcb36df5b8b
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-17 22:47:14 +0900

    upd 3.6 -> 3.7

--- util/banner/script.js
+++ util/banner/script.js
@@ -1,11 +1,16 @@
-const list = [1, 2, 3, 4, 5]; //Current banner
+const list = [6, 7, 8, 9, 10]; //Current banner
 
 const map = {
     1: ["Nilou", "-920px", "-140px"],
     2: ["Nahida", "-900px", "-250px"],
     3: ["Layla", "-930px", "-140px"],
     4: ["Shinobu", "-990px", "-80px"],
-    5: ["Dori", "-960px", "-80px"]
+    5: ["Dori", "-960px", "-80px"],
+    6: ["Yae", "-890px", "-80px"], //八重神子
+    7: ["Yoimiya", "-950px", "-100px"], //宵宮
+    8: ["Momoka", "-920px", "-80px"], //きらら
+    9: ["Chongyun", "-950px", "-90px"], //重雲
+    10: ["Yunjin", "-950px", "-80px"] //雲菫
 }
 
 for (let n = 1; n < 6; n++) {
