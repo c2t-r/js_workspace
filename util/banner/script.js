2f951639a0c1e4542b523c977b265e6ec92226b7
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-24 00:09:51 +0900

    hotfix

--- util/banner/script.js
+++ util/banner/script.js
@@ -2,9 +2,6 @@ import mapping from './mapping.json' assert {type: 'json'};
 
 const list = ["心海", "放浪者", "トーマ", "煙緋", "ファルザン"];
 
-const bgimage = "https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_Kokomi_P.png";
-
-
 window.onload = function() {
     for (let n = 1; n < 6; n++) {
         const ch = document.querySelector(`.c${n}`);
@@ -12,6 +9,4 @@ window.onload = function() {
         ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
         ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
     }
-    bg = document.querySelector(`.bg`);
-    bg.style.backgroundImage = `url(${bgimage})`;
 }
