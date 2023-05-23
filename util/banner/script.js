8e0824a9e21f4a43069f20ecbb2855abaa7926cd
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-23 23:17:19 +0900

    hotfix

--- util/banner/script.js
+++ util/banner/script.js
@@ -4,12 +4,15 @@ const list = ["心海", "放浪者", "トーマ", "煙緋", "ファルザン"];
 
 const bgimage = "https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_Kokomi_P.png";
 
-bg = document.querySelector(".bg");
-bg.style.backgroundImage = `url(${bgimage})`;
 
-for (let n = 1; n < 6; n++) {
-    const ch = document.querySelector(`.c${n}`);
+window.onload = function() {
+    bg = document.querySelector(".bg");
+    bg.style.backgroundImage = `url(${bgimage})`;
 
-    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
-    ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
+    for (let n = 1; n < 6; n++) {
+        const ch = document.querySelector(`.c${n}`);
+
+        ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
+        ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
+    }
 }
