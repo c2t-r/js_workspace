c3e0942a8f51c0d9dd2895702bc2cd212e85a633
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-23 23:56:22 +0900

    hotfix

--- util/banner/script.js
+++ util/banner/script.js
@@ -6,13 +6,12 @@ const bgimage = "https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_Kokomi_P
 
 
 window.onload = function() {
-    //bg = document.querySelector(".bg");
-    //bg.style.backgroundImage = `url(${bgimage})`;
-
     for (let n = 1; n < 6; n++) {
         const ch = document.querySelector(`.c${n}`);
 
         ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
         ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
     }
+    bg = document.querySelector(".bg");
+    bg.style.backgroundImage = `url(${bgimage})`;
 }
