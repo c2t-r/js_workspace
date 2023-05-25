9dd372413b7695bf064e5c6d86136c7b6aa40622
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-25 21:14:58 +0900

    add custom credit

--- util/banner/script.js
+++ util/banner/script.js
@@ -2,16 +2,18 @@ import mapping from './mapping.json' assert {type: 'json'};
 
 const list = ["心海", "放浪者", "トーマ", "煙緋", "ファルザン"];
 
+let creditText = 'c2t-r.github.io/js_workspace';
+
 window.onload = function() {
     for (let n = 1; n < 6; n++) {
         const ch = document.querySelector(`.c${n}`);
 
-        ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
-        ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
+        ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping["character"][list[n-1]][0]}.png)`;
+        ch.style.backgroundPosition = `${mapping["character"][list[n-1]][1]} ${mapping["character"][list[n-1]][2]}`;
     }
     for (let n = 0; n < 6; n++) {
         let select = document.querySelectorAll(`.search_box`)[n];
-        for (const entry of Object.entries(mapping)) {
+        for (const entry of Object.entries(mapping["character"])) {
             const option = document.createElement("option");
             const text = document.createTextNode(entry[0]);
             option.value = entry[0];
@@ -21,17 +23,28 @@ window.onload = function() {
     }
 }
 
-function changeCharacter(param) {
+function changeCharacter() {
     let ch = document.querySelector(`.c${this.name + 1}`);
     let c_name = document.querySelectorAll(`.search_box`)[this.name].value;
-    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[c_name][0]}.png)`;
-    ch.style.backgroundPosition = `${mapping[c_name][1]} ${mapping[c_name][2]}`;
+    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping["character"][c_name][0]}.png)`;
+    ch.style.backgroundPosition = `${mapping["character"][c_name][1]} ${mapping["character"][c_name][2]}`;
 }
 
-function changeNamecard(param) {
+function changeNamecard() {
     let ch = document.querySelector(`.bg`);
     let c_name = document.querySelectorAll(`.search_box`)[5].value;
-    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_${mapping[c_name][0]}_P.png)`;
+    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_${mapping["namecard"][c_name]}_P.png)`;
+}
+
+function changeBgColor() {
+    let clr = document.querySelector("#bgcolor").value;
+    document.querySelector("body").style.backgroundColor = clr;
+    document.querySelectorAll(".table_wrap")[0].style.backgroundColor = clr;
+    document.querySelectorAll(".table_wrap")[1].style.backgroundColor = clr;
+}
+
+function changeCreditText() {
+    creditText = document.querySelector("#creditText").value;
 }
 
 function tocanvas() {
@@ -39,13 +52,13 @@ function tocanvas() {
     if (canvas !== null) {
         canvas.remove();
     }
-    html2canvas(document.querySelector(".banner_wrap"), {proxy: true, useCORS: true, onrendered: function(canvas) { canvas.toDataURL();} }).then(canvas => {
+    html2canvas(document.querySelector(".table_wrap"), {proxy: true, useCORS: true, onrendered: function(canvas) { canvas.toDataURL();} }).then(canvas => {
         let location = document.querySelector(".main");
         location.appendChild(canvas);
         var ctx = canvas.getContext('2d');
         ctx.font = "24px serif";
         ctx.fillStyle = "#8dd5ff";
-        ctx.fillText('c2t-r.github.io/js_workspace', 545, 590);
+        ctx.fillText(creditText, 550, 595);
         document.querySelector("#dlbutton").setAttribute("style", "visibility: visible;");
     });
 }
@@ -69,5 +82,7 @@ document.querySelectorAll(".search_box")[2].addEventListener('change', {name: ch
 document.querySelectorAll(".search_box")[3].addEventListener('change', {name: change4, handleEvent: changeCharacter});
 document.querySelectorAll(".search_box")[4].addEventListener('change', {name: change5, handleEvent: changeCharacter});
 document.querySelectorAll(".search_box")[5].addEventListener('change', changeNamecard);
+document.querySelector("#bgcolor").addEventListener('input', changeBgColor);
+document.querySelector("#creditText").addEventListener('input', changeCreditText);
 document.querySelector("#Generator").addEventListener('click', tocanvas);
 document.querySelector("#dlbutton").addEventListener('click', download_canvas);
