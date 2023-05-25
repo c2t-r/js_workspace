363ef2bdb54f80e6ffe941dadd76bbb8bd452859
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-05-25 16:56:15 +0900

    made Change Namecard available

--- util/banner/script.js
+++ util/banner/script.js
@@ -9,8 +9,8 @@ window.onload = function() {
         ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[list[n-1]][0]}.png)`;
         ch.style.backgroundPosition = `${mapping[list[n-1]][1]} ${mapping[list[n-1]][2]}`;
     }
-    for (let n = 1; n < 6; n++) {
-        let select = document.querySelector(`.s${n}`);
+    for (let n = 0; n < 6; n++) {
+        let select = document.querySelectorAll(`.search_box`)[n];
         for (const entry of Object.entries(mapping)) {
             const option = document.createElement("option");
             const text = document.createTextNode(entry[0]);
@@ -22,13 +22,16 @@ window.onload = function() {
 }
 
 function changeCharacter(param) {
-    let ch = document.querySelector(`.c${this.name}`);
-    let c_name = document.querySelector(`.s${this.name}`).value;
-    if (mapping[c_name][1] == null) {
-        return;
-    }
+    let ch = document.querySelector(`.c${this.name + 1}`);
+    let c_name = document.querySelectorAll(`.search_box`)[this.name].value;
     ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/UI_Gacha_AvatarImg_${mapping[c_name][0]}.png)`;
     ch.style.backgroundPosition = `${mapping[c_name][1]} ${mapping[c_name][2]}`;
+}
+
+function changeNamecard(param) {
+    let ch = document.querySelector(`.bg`);
+    let c_name = document.querySelectorAll(`.search_box`)[5].value;
+    ch.style.backgroundImage = `url(https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_${mapping[c_name][0]}_P.png)`;
 }
 
 function tocanvas() {
@@ -54,15 +57,16 @@ function download_canvas() {
     link.click();
 }
 
-let change1 = "1";
-let change2 = "2";
-let change3 = "3";
-let change4 = "4";
-let change5 = "5";
-document.querySelector(".s1").addEventListener('change', {name: change1, handleEvent: changeCharacter});
-document.querySelector(".s2").addEventListener('change', {name: change2, handleEvent: changeCharacter});
-document.querySelector(".s3").addEventListener('change', {name: change3, handleEvent: changeCharacter});
-document.querySelector(".s4").addEventListener('change', {name: change4, handleEvent: changeCharacter});
-document.querySelector(".s5").addEventListener('change', {name: change5, handleEvent: changeCharacter});
+let change1 = 0;
+let change2 = 1;
+let change3 = 2;
+let change4 = 3;
+let change5 = 4;
+document.querySelectorAll(".search_box")[0].addEventListener('change', {name: change1, handleEvent: changeCharacter});
+document.querySelectorAll(".search_box")[1].addEventListener('change', {name: change2, handleEvent: changeCharacter});
+document.querySelectorAll(".search_box")[2].addEventListener('change', {name: change3, handleEvent: changeCharacter});
+document.querySelectorAll(".search_box")[3].addEventListener('change', {name: change4, handleEvent: changeCharacter});
+document.querySelectorAll(".search_box")[4].addEventListener('change', {name: change5, handleEvent: changeCharacter});
+document.querySelectorAll(".search_box")[5].addEventListener('change', changeNamecard);
 document.querySelector("#Generator").addEventListener('click', tocanvas);
 document.querySelector("#dlbutton").addEventListener('click', download_canvas);
