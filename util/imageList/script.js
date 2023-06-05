ee532fbd41c58cf0659e683efaffa5870439081e
c2tr <80561604+c2t-r@users.noreply.github.com>
2023-06-05 09:07:33 +0900

    hotfix

--- util/imageList/script.js
+++ util/imageList/script.js
@@ -4,20 +4,38 @@ const limit = params.get('limit');
 const user = params.get('user');
 const repo = params.get('repo');
 const dir = params.get('dir');
+
+const allow_exts = new Array('.jpg', '.jpeg', '.png');
+
 const input = document.querySelector(".setLimit");
 
 window.onload = async function() {
-    const apiUrl = `https://api.github.com/repos/${user}/${repo}/contents/${dir}`;
-    const responce = await (await fetch(apiUrl)).json();
-
     if (document.querySelector(".imgs") !== null){
         document.querySelector(".imgs").remove()
+    }
+
+    const apiUrl = `https://api.github.com/repos/${user}/${repo}/contents/${dir}`;
+    const option = {
+        method: "GET"
+    };
+    let json;
+    const response = await fetch(apiUrl, option);
+    if (response.ok) {
+        json = await response.json();
+    } else {
+        console.error(`[ERROR] GET ${response.url} : ${response.status}`);
     }
 
     const imgs = document.createElement("div");
     imgs.classList.add("imgs");
 
-    for (const entry of responce) {
+    for (const entry of json) {
+        if (entry["download_url"] == null){
+            continue;
+        }
+        if (!checkExt(entry["name"])) {
+            continue;
+        }
         const img = document.createElement("img");
         img.classList.add("img");
 
@@ -29,6 +47,15 @@ window.onload = async function() {
     document.querySelector(".main").appendChild(imgs);
     setLimit(limit);
     input.value = limit;
+}
+
+function checkExt(filename) {
+    const ext = filename.slice(filename.lastIndexOf(".")).toLowerCase();
+    if (allow_exts.indexOf(ext) === -1) {
+        console.log(`${filename} : ${ext} is not allowed.`)
+        return false;
+    }
+	return true;
 }
 
 function setLimit(limit) {
